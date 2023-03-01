30cad607b48d760cd03713166809586b09e5d8ab
Tamara Saadeh <tamara.saadeh95@gmail.com>
2023-03-01 16:34:17 -0500

    simple array loop

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 console.log("Hello Github");
 
-let x = 15;
+const firstNames = ["Jon", "Arya", "Jamie"];
+const lastNames = ["Snow", "Stark", "Lannister"];
+const places = ["The Wall", "Winterfell", "Kings Landing"];
+
+
+const bios = [];
+
+// Loop through your arrays and store the following strings in the bios array:
+// 'My name is Jon Snow and I am from The Wall'
+// 'My name is Arya Stark and I am from Winterfell'
+// 'My name is Jamie Lannister and I am from Kings Landing'
+
+// ADD CODE HERE
+
+for(let idx = 0; idx < firstNames.length; idx++)
+  {
+    bios.push(`My name is ${firstNames[idx]} ${lastNames[idx]} and I am from ${places[idx]}`);
+  }
+
+bios.forEach(function(bio)
+{
+    console.log(bio);
+})
